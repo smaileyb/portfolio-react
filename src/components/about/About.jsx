9384b63f28f7c49df9f84a3bb12bff518264f212
Smailey Alexandre Behm <89866949+smaileyb@users.noreply.github.com>
2022-07-15 16:27:04 -0300

    Componentes Nav e About

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.jpg'
+import { BiAward } from 'react-icons/bi'
+import { FiUsers } from 'react-icons/fi'
+import { VscFolderLibrary } from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+      <h5>Get to Know</h5>
+      <h2>About me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+        <div className="about__me-image">
+          <img src={ME} alt="About Image" />
+        </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+            <BiAward className='about__icon' />
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+            <article className="about__card">
+            <FiUsers className='about__icon' />
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+            <article className="about__card">
+            <VscFolderLibrary className='about__icon' />
+              <h5>Projects</h5>
+              <small>80+ Completed</small>
+            </article>
+          </div>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum, beatae enim quod quam nihil doloribus voluptatem aperiam aspernatur corrupti, nisi cum animi sapiente quia id esse necessitatibus, provident cupiditate tenetur!</p>
+          <a href="#contact" className='btn btn-primary' >Let's Talk</a>
+
+        </div>
+      </div>
+
+
+
+    </section>
   )
 }
 
